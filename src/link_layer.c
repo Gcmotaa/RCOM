bcb7d55d8503be6a3e0844bc1a1c9141015ecbae
Leonor Azevedo <leonor.costa.azevedo@gmail.com>
2025-10-17 11:14:46 +0100

    started llread

--- src/link_layer.c
+++ src/link_layer.c
@@ -6,6 +6,54 @@
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
+typedef enum {
+    INITIAL,
+    F_RECEIVED,
+    A_RECEIVED,
+    C_RECEIVED,
+    BCC1_RECEIVED,
+    RECEIVING_DATA,
+    END
+} recieving_I_state;
+
+int Ns = 0;
+
+void updateRecievingIState(recieving_I_state *state, unsigned char byte, unsigned char* header){
+    switch (*state)
+    {
+    case INITIAL:
+        if (byte == F){
+            *state = F_RECEIVED;
+        }
+        break;
+
+    case F_RECEIVED:
+        if (byte != F){
+            *state = A_RECEIVED;
+            *header = byte;
+        }
+        break;
+
+    case A_RECEIVED:
+        *state = C_RECEIVED;
+        *(header+1) = byte;
+        break;
+
+    case C_RECEIVED:
+        *state = BCC1_RECEIVED;
+        break;
+
+    case RECEIVING_DATA:
+        if(byte == F){
+            *state = END;
+        }
+        break;
+    
+    default:
+        break;
+    }
+}
+
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
@@ -31,7 +79,29 @@ int llwrite(const unsigned char *buf, int bufSize)
 ////////////////////////////////////////////////
 int llread(unsigned char *packet)
 {
-    // TODO: Implement this function
+    recieving_I_state state = INITIAL;
+    unsigned char *header;
+
+    while (state != END){
+        unsigned char byte;
+        int nBytes = readByteSerialPort(&byte);
+
+        if(nBytes > 0){
+            updateRecievingIState(&state, byte, header);
+
+            if(state == BCC1_RECEIVED && byte == (*header ^ *(header+1))){ //check if bcc1 is correct
+                if(*(header+1) == Ns << 7){ //check if the ns we received are the one we are expecting
+                    state = RECEIVING_DATA;
+                }
+                else{ //it is not the one we are expecting
+
+                }
+            }
+            else if (state == BCC1_RECEIVED){
+                state = INITIAL;
+            }
+        }
+    }
 
     return 0;
 }
