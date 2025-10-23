8e9bf985ab018add324e239e2f3512390fd714be
Leonor Azevedo <leonor.costa.azevedo@gmail.com>
2025-10-23 21:12:17 +0100

    started application layer

--- src/application_layer.c
+++ src/application_layer.c
@@ -1,11 +1,25 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
 
 #include <stdio.h>
+#include <string.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO: Implement this function
+    LinkLayer ll;
+
+    strcpy(ll.serialPort, serialPort);
+    ll.role = (strcmp(role, "tx") == 0 ? LlTx : LlRx);
+    ll.baudRate = baudRate;
+    ll.nRetransmissions = nTries;
+    ll.timeout = timeout;
+
+    //open the file
+    FILE* readFile = fopen(filename, "r");
+    if(readFile == NULL) return -1;
+
+    if(llopen(ll) == -1) return -1; //open comunication
 }
